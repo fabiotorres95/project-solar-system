07a9d9c9e8b77f8b064c67e73a89eb43979ec59a
fabiotorres95 <engfabiotorres@outlook.com>
2023-01-24 16:16:29 -0300

    Missions uses MissionCard for each mission

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        { missions.map((obj) => (
+          <MissionCard
+            name={ obj.name }
+            year={ obj.year }
+            country={ obj.country }
+            destination={ obj.destination }
+            key={ obj.name }
+          />
+        ))}
       </div>
     );
   }
